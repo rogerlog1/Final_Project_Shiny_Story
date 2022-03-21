5fbd107a4eebea536d3c430a85c684b0ac361719
rogerlog1 <rogerlog@mail.gvsu.edu>
2022-03-21 23:12:07 +0000

    I made good progress today.

--- Draft1/Draft1.R
+++ Draft1/Draft1.R
@@ -27,28 +27,32 @@ ui <- fluidPage(
                               "old"),
                   selected = "young"),
       selectInput("var4",
-                  label = "Gender",
+                  label = "What is your gender?",
                   choices = c("man",
                               "woman"),
                   selected = "man"),
+      numericInput("Age","How old are you?", 20, min=1, max=100),
+      textInput("Name", "What is your name?", value = "", width = NULL, placeholder = NULL),
+     
     ),
     
     mainPanel(
       textOutput("selected_var"),
       br(),
-      p("Setting setting setting."),
       textOutput("selected_var2"),
+      br(),
+      textOutput("age"),
+      br(),
+      textOutput("name"),
     )
   )
 )
 server <- function(input, output) {
   
-  output$selected_var <- renderText({
-    paste("Your character has", input$var, "hair and", input$var2, "eyes.")
-  })
-  output$selected_var2 <- renderText({ 
-    paste("Your character is a(n)", input$var3, input$var4, "on their way to a local park.")
-  })
+  output$selected_var <- renderText({paste("Your character has", input$var, "hair and", input$var2, "eyes.")})
+  output$selected_var2 <- renderText({ paste("Your character is a(n)", input$var3, input$var4, "on their way to a local park.")})
+  output$age <- renderText({ paste("You are",input$Age,"years old.") })
+  output$name <- renderText({ paste("Your name is",input$Name) })
   
 }
 
